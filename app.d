519ec55a9f52d49581292fe4316f2b2a9ef59422
Lein Matsumaru <yharuhi39@gmail.com>
2016-03-29 22:36:51 +0000

    Initial commit outline of wxApp created.

---
+++ app.d
@@ -0,0 +1,81 @@
+class wxApp : wxAppConsole
+{
+public:
+  /**
+     Constructor.
+   */
+  this();
+  /**
+     Destructor.
+   */
+  ~this();
+  @property
+  {
+    /**
+       Returns:
+       Display mode that is used use.
+     */
+    wxVideoMode DisplayMode();
+    /**
+       RETURN:
+       Display mode that is used use.
+     */
+    bool ExitOnFrameDelete();
+    wxLayoutDirection LayotDirection();
+    bool UseBestVisual();
+    wxWindow TopWindow();
+    bool Active();
+    void DisplayMode(wxVideoMode info) throws Exception;
+    void ExitOnFrameDelete(bool flag);
+    void NativeTheme(wxString theme) throws Exception;
+    void TopWindow(wxWindow window) throws Exception;
+    void UseBestVisual(bool flag, bool forceTrueColor=false);
+  }
+
+  version(OSX)
+  {
+    void MacNewFile();
+    void MacOpenFiles(wxArrayString fileNames);
+    void MacOpenFile(wxString fileName);
+    void MacOpenURL(wxString url);
+    void MacPrintFile(wxString fileName);
+    void MacReopenApp();
+    @property void OSXGUIApplication();
+  }
+}
+
+class wxAppConsole
+{
+}
+
+class wxCmdLineParser
+{
+}
+
+class wxDynamicLibrary
+{
+}
+
+class wxEventLoopBase
+{
+}
+
+class wxGUIEventLoop
+{
+}
+
+class wxInitializer
+{
+}
+
+class wxModule
+{
+}
+
+class wxProcess
+{
+}
+
+class wxSingleInstanceChecker
+{
+}
