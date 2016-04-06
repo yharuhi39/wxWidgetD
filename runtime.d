8e6156deb6bfcbb9f0dad7ef629a37a119e88813
Lein Matsumaru <yharuhi39@gmail.com>
2016-04-06 14:02:11 +0000

    add runtimeFile as file info

--- runtime.d
+++ runtime.d
@@ -1,20 +1,61 @@
 
-class wxObject
+class wxObject : Object
 {
 public:
-  this();
-  ~this();
-  void Ref(wxObject clone);
-  void UnRef(wxObject object);
+  const wxClassInfo[] classParents = { null };
+  this(){ this.RefData = null; }
+  ~this(){ this.UnRef(); }
+  void Ref(wxObject clone)
+  {
+    if(this.RefData == clone.RefData) return;
+    this.Unref();
+    if(clone.RefData)
+      {
+	this.RefData = clone.RefData;
+	this.RefData.IncRef();
+      }
+  }
+  
+  void UnRef()
+  {
+    if(this.RefData)
+      {
+	this.RefData.DecRef();
+	this.RefData = null;
+      }
+  }
+
+  void UnShare(){ this.AllocExclusive(); }
+
   @property
   {
-    @override auto dup(wxObject[] a);
-    wxClassInfo ClassInfo();
-    wxObjectRefData RefData();
-    bool IsKindOf(wxClassInfo info);
-    bool IsSameAs(wxClassInfo info);
-    
+    override auto dup(wxObject[] a);
+    wxClassInfo ClassInfo(){ return this.classinfo; }
+    wxObjectRefData RefData(){ return this.refData; }
+    void RefData(wxObjectRefData data){ this.refData = data; }
   }
+
+  bool IsKindOf(wxClassInfo info){ return (this.ClassInfo) ? this.ClassInfo.IsKindOf(info) : false; }
+  bool IsSameAs(wxClassInfo info){ return this.RefData == o.RefData}
+
+  override bool opEquals(Object info){ return this.ClassInfo == cast(wxClassInfo)info; }
+
+  override void destroy(wxObject buf) throws Exception { throw new Exception("Error"); }
+protected:
+  wxObjectRefData refData;
+  
+  void AllocExclusive()
+  {
+    if(!this.RefData)
+      {
+	this.RefData = this.CreateRefData();
+      }
+  }
+  const wxObjectRefData CreateRefData()
+  {
+    return null;
+  }
+  const wxObjectRefData CloneRefData(wxObjectRefData data);
   
 }
 
@@ -27,9 +68,27 @@ class wxObjectRefData(T)
 }
 
 class wxRefCounter
-{
+{w
 }
 
 class wxClassInfo
 {
+public:
+  this(const wxString className, wxClassInfo baseClass1, wxClassInfo baseClass2, int size, wxObjectConstructorFn fn);
+  const wxObject createObject();
+  @property
+  {
+    wxString BaseClassName1();
+    wxString BaseClassName1();
+    wxString ClassName();
+    int Size();
+    bool IsDynamic();
+    bool IsKindOf(wxClassInfo info);
+  }
+
+  static wxClassInfo FindClass(wxString className)
+  {
+    if(
+  }
+  
 }
